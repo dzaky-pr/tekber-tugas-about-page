a8ad43ac84e842e2c249fa52ffac1c4a051998f7
dzaky-pr <dzaky.pr.mulyono97@gmail.com>
2024-11-12 18:25:29 +0700

    feat: version 1.2.0 fix overflowed text and screen

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,7 @@ class MyProfilePage extends StatelessWidget {
           style: const TextStyle(
               fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white),
         ),
-        const SizedBox(height: 4), // Jarak antara angka dan label
+        const SizedBox(height: 4),
         Text(
           label,
           style: const TextStyle(fontSize: 14, color: Colors.white),
@@ -45,22 +45,22 @@ class MyProfilePage extends StatelessWidget {
     );
   }
 
-  Widget _buildButtonColumn(IconData icon, {bool active = false}) {
+  Widget _buildButtonColumn(IconData icon,
+      {required Color iconColor, bool active = false, double iconSize = 24.0}) {
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
         Icon(
           icon,
-          color: active
-              ? Colors.white
-              : Colors.grey.shade600, // Warna berubah jika aktif
+          color: iconColor,
+          size: iconSize, // Gunakan iconSize di sini
         ),
-        const SizedBox(height: 8), // Jarak antara ikon dan garis bawah
+        const SizedBox(height: 8),
         if (active)
           Container(
             width: 24,
             height: 2,
-            color: Colors.white, // Warna garis bawah jika aktif
+            color: Colors.white,
           ),
       ],
     );
@@ -74,6 +74,7 @@ class MyProfilePage extends StatelessWidget {
     double textSize = 12.0,
   }) {
     return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Icon(
           icon,
@@ -82,11 +83,14 @@ class MyProfilePage extends StatelessWidget {
           semanticLabel: 'Icon Details',
         ),
         const SizedBox(width: 4), // Jarak antara ikon dan teks
-        Text(
-          text,
-          style: TextStyle(
-            fontSize: textSize,
-            color: Colors.white,
+        Flexible(
+          // Tambahkan Flexible di sini
+          child: Text(
+            text,
+            style: TextStyle(
+              fontSize: textSize,
+              color: Colors.white,
+            ),
           ),
         ),
       ],
@@ -97,112 +101,143 @@ class MyProfilePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          'dzaky.rifai',
-          style: TextStyle(
-              fontWeight: FontWeight.bold, color: Colors.white, fontSize: 16),
-        ),
-        backgroundColor: Colors.grey.shade900,
-      ),
-      body: Center(
-        child: ConstrainedBox(
+        title: ConstrainedBox(
           constraints: const BoxConstraints(
             maxWidth: 600,
           ),
           child: Padding(
             padding: const EdgeInsets.all(24.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment:
+                  CrossAxisAlignment.center, // Align items vertically center
               children: [
-                Row(
-                  children: [
-                    const CircleAvatar(
-                      radius: 48,
-                      backgroundImage: NetworkImage('images/image.png'),
-                    ),
-                    const SizedBox(width: 24),
-                    Expanded(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          _buildStatColumn('Post', '10'),
-                          _buildStatColumn('Followers', '2081'),
-                          _buildStatColumn('Following', '2087'),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                    height: 16), // Jarak antara foto dan detail bawah
                 const Text(
-                  'Dzaky Purnomo Rifa\'i',
+                  'dzaky.rifai',
                   style: TextStyle(
                     fontWeight: FontWeight.bold,
-                    fontSize: 16,
                     color: Colors.white,
+                    fontSize: 16,
                   ),
                 ),
-                const SizedBox(height: 8),
-                const Text(
-                  'Beasiswa Unggulan Awardee Kemendikbudristek RI 2023 | Undergraduate Information System Student at Institut Teknologi Sepuluh Nopember',
-                  style: TextStyle(
-                    fontSize: 12,
-                    color: Colors.white70,
-                    fontWeight: FontWeight.normal,
-                  ),
-                ),
-                const SizedBox(height: 8),
-
-                const SizedBox(height: 4),
-                Column(
-                  children: [
-                    _buildIconTextRow(
-                      icon: Icons.account_circle,
-                      text: 'NRP: 5026221085',
-                      iconColor: Colors.white,
-                    ),
-                    const SizedBox(height: 8),
-                    _buildIconTextRow(
-                      icon: Icons.celebration,
-                      text:
-                          'Funfact: Frontend Developer, aspiring Techpreneur, and lifelong learner.',
-                      iconColor: Colors.white,
-                    ),
-                  ],
-                ),
-
-                const SizedBox(height: 36),
-
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    _buildButtonColumn(Icons.apps, active: true),
-                    _buildButtonColumn(Icons.video_library),
-                    _buildButtonColumn(Icons.bookmark_border),
-                  ],
-                ),
-
-                // Grid view untuk menampilkan gambar lokal
-                const SizedBox(height: 12), // Jarak sebelum grid
-                GridView.builder(
-                  shrinkWrap: true, // Agar grid tidak mengambil ruang penuh
-                  physics:
-                      const NeverScrollableScrollPhysics(), // Non-scrollable grid
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 3, // 3 kolom
-                    crossAxisSpacing: 8, // Jarak antar kolom
-                    mainAxisSpacing: 8, // Jarak antar baris
-                  ),
-                  itemCount: imageUrls.length,
-                  itemBuilder: (context, index) {
-                    return Image.asset(
-                      imageUrls[index],
-                      fit: BoxFit.cover, // Agar gambar tetap pas di dalam grid
-                    );
-                  },
+                const SizedBox(width: 4),
+                Icon(
+                  Icons.verified,
+                  color: Colors.blue,
+                  size: 16,
                 ),
               ],
+            ),
+          ),
+        ),
+        backgroundColor: Colors.grey.shade900,
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Center(
+            child: ConstrainedBox(
+              constraints: const BoxConstraints(
+                maxWidth: 600,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(24.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        const CircleAvatar(
+                          radius: 48,
+                          backgroundImage: NetworkImage('images/image.png'),
+                        ),
+                        const SizedBox(width: 20),
+                        Expanded(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              _buildStatColumn('Post', '11'),
+                              _buildStatColumn('Followers', '2081'),
+                              _buildStatColumn('Following', '2087'),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                        height: 16), // Jarak antara foto dan detail bawah
+                    const Text(
+                      'Dzaky Purnomo Rifa\'i',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    const Text(
+                      'Beasiswa Unggulan Awardee Kemendikbudristek RI 2023 | Undergraduate Information System Student at Institut Teknologi Sepuluh Nopember',
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.white70,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+
+                    const SizedBox(height: 4),
+                    Column(
+                      children: [
+                        _buildIconTextRow(
+                          icon: Icons.account_circle,
+                          text: 'NRP: 5026221085',
+                          iconColor: Colors.white,
+                        ),
+                        const SizedBox(height: 8),
+                        _buildIconTextRow(
+                          icon: Icons.celebration,
+                          text:
+                              'Funfact: Frontend Developer aspiring to become a Techpreneur. A lifelong learner committed to making a positive impact on many people.',
+                          iconColor: Colors.white,
+                        ),
+                      ],
+                    ),
+
+                    const SizedBox(height: 36),
+
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        _buildButtonColumn(Icons.apps,
+                            iconColor: Colors.white, active: true),
+                        _buildButtonColumn(Icons.video_library,
+                            iconColor: Colors.grey.shade600),
+                        _buildButtonColumn(Icons.bookmark_border,
+                            iconColor: Colors.grey.shade600),
+                      ],
+                    ),
+
+                    const SizedBox(height: 12),
+                    GridView.builder(
+                      shrinkWrap: true,
+                      physics:
+                          const NeverScrollableScrollPhysics(), // Non-scrollable grid
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 3,
+                        crossAxisSpacing: 8,
+                        mainAxisSpacing: 8,
+                      ),
+                      itemCount: imageUrls.length,
+                      itemBuilder: (context, index) {
+                        return Image.asset(
+                          imageUrls[index],
+                          fit: BoxFit.cover,
+                        );
+                      },
+                    ),
+                  ],
+                ),
+              ),
             ),
           ),
         ),
